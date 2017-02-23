0cebaae3ff9cdb2da8f4971385a1b5b74d546265
DawsonGeist <Dawson.geist@gmail.com>
2017-02-22 20:16:29 -0500

    Add files via upload

--- baboloni.java
+++ baboloni.java
@@ -1,53 +1,41 @@
 package hwTwo;
-
+import java.util.*;
 public class baboloni 
 {
-	//the variable containing the original statement x/2
-	private static float dummyGuess;
-	
-	//the number being square rooted
-	private static float userEntry;
-	
-	//variable that holds the calculated root
-	private static float calculatedAnswer;
-	
-	//error allowed
-	private static double error;
-	
-	//percent error between dummmyGuess^2 and userEntry
+	private static double userFirstEntry;
+	private static double userGivenError;
 	private static double calculatedError;
+	private static double developingAnswer;
+	private static boolean stopLoop = false;
 	
 	
-	//user Entry = the calculated root^2
-	//use that to derive the threshold for error
-	public static float babyRoot(float entry, double tollerance)
-	{
-		dummyGuess= entry/2;
-		System.out.print("dummyGuess is "+dummyGuess);
-		calculatedError = entry - (Math.pow(dummyGuess,2));
-		System.out.print("calculatedError is "+calculatedError);
-		
-		//too much error present
-		if(tollerance<calculatedError)
-		{
-			//(1/2 * guess + userEntry/guess) Babylons method
-			calculatedAnswer = (float) (.5*(dummyGuess+entry/dummyGuess));
-			System.out.println(calculatedAnswer);
-			return babyRoot(calculatedAnswer,error);
-		}
-		else
-		{
-			return calculatedAnswer;
-		}
-		
-	}
 	
 	public static void main(String[] args) 
 	{
-		userEntry = 25;
-		error = 0;
-		System.out.println(babyRoot(userEntry,error));
+		Scanner keyboard = new Scanner(System.in);
+		//prompt user
+		System.out.println("Please enter the value you want to find the square root of");
+		//save answer and preserve it for later checking purposes
+		System.out.print("Value: ");
+		userFirstEntry = keyboard.nextDouble();
+		//after the user hits enter it will display Value: x
+		System.out.print(userFirstEntry+"\n");
+		System.out.println("Please enter the percent for acceptable error");
+		userGivenError = keyboard.nextDouble();
 
+		//this is the first value entered
+		developingAnswer/=2;
+		
+		//start the Babylonian Method
+		do
+		{
+		//call rootFinder - updates developing answer
+			//test return value to see if it is less than userGivenError
+				//if true stopLoop = true
+				//if false do nothing
+					//loop restarts with the developing answer updated
+		}while(!stopLoop);
+		
 	}
 
 }
