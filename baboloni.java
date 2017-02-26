1d96f216b3c0ccea81d75418fe87c3ace30ede59
DawsonGeist <Dawson.geist@gmail.com>
2017-02-25 20:26:58 -0500

    Add files via upload

--- baboloni.java
+++ baboloni.java
@@ -2,40 +2,49 @@ package hwTwo;
 import java.util.*;
 public class baboloni 
 {
-	private static double userFirstEntry;
-	private static double userGivenError;
-	private static double calculatedError;
-	private static double developingAnswer;
-	private static boolean stopLoop = false;
-	
 	
 	
 	public static void main(String[] args) 
 	{
+		double userFirstEntry=0;
+		double userGivenError=0;;
 		Scanner keyboard = new Scanner(System.in);
 		//prompt user
 		System.out.println("Please enter the value you want to find the square root of");
 		//save answer and preserve it for later checking purposes
 		System.out.print("Value: ");
 		userFirstEntry = keyboard.nextDouble();
-		//after the user hits enter it will display Value: x
-		System.out.print(userFirstEntry+"\n");
+		if(userFirstEntry<0)
+		{	
+			do
+			{
+				System.out.println("Invalid input, input must be greater than 0");
+				System.out.println("Please enter the value you want to find the square root of");
+				System.out.print("Value: ");
+				userFirstEntry = keyboard.nextDouble();
+			}while(userFirstEntry<0);
+		}
+		 
+		
+		
+		//prompt
 		System.out.println("Please enter the percent for acceptable error");
-		userGivenError = keyboard.nextDouble();
-
-		//this is the first value entered
-		developingAnswer/=2;
 		
-		//start the Babylonian Method
-		do
-		{
-		//call rootFinder - updates developing answer
-			//test return value to see if it is less than userGivenError
-				//if true stopLoop = true
-				//if false do nothing
-					//loop restarts with the developing answer updated
-		}while(!stopLoop);
+		//fail safe
+		if(keyboard.nextDouble()<0)
+		{	
+			do
+			{
+				System.out.println("Invalid input, input must be greater than 0");
+				System.out.println("Please enter the percent for acceptable error");
+				userGivenError = keyboard.nextDouble();
+			}while(userFirstEntry<0);
+		}
 		
+		
+		keyboard.close();
+		rootFinder question1 = new rootFinder(10,2);
+		System.out.println(question1.generateFirstGuess(question1.originalValue));
 	}
 
 }
